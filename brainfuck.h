e503f051ab549482ef0501b543b5ce6de3fbecc7
Juroupi <juroupi.lo@gmail.com>
2020-11-22 01:57:54 +0100

    simplification de l'interface

--- brainfuck.h
+++ brainfuck.h
@@ -3,17 +3,15 @@
 
 typedef struct {
 
-	int array_size;
+	unsigned int array_size;
 
 	unsigned char* array;
 
 	unsigned char* ptr;
 
-	int* code_stack;
-
 } brainfuck_st;
 
-int bf_init(brainfuck_st* bf, int array_size);
+int bf_init(brainfuck_st* bf, unsigned int array_size);
 
 int bf_exec(brainfuck_st* bf, char* code);
 
