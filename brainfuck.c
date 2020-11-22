94f422df94ba4c482ed4c276f8e478f2a9023b30
Juroupi <juroupi.lo@gmail.com>
2020-11-22 01:58:48 +0100

    implementation init() et clear()

--- brainfuck.c
+++ brainfuck.c
@@ -1,1 +1,21 @@
 #include "brainfuck.h"
+
+#include <stdlib.h>
+
+int bf_init(brainfuck_st* bf, unsigned int array_size) {
+	
+	bf->array_size = array_size;
+
+	bf->array = malloc((size_t)bf->array_size);
+	if (bf->array == NULL) {
+		return 0;
+	}
+
+	bf->ptr = bf->array;
+
+	return 1;
+}
+
+void bf_clear(brainfuck_st* bf) {
+	free(bf->array);
+}
